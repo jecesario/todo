2186ddcf9d355edb321749804826ab79f7e0c7f7
jecesario <jean.cesario@thoughworks.com>
2022-06-14 21:31:18 -0300

    fix(project): fix jwt timing and SecurityConfig.java

--- src/main/java/com/easytech/todo/exceptions/AuthenticationFailureException.java
+++
@@ -1,7 +0,0 @@
-package com.easytech.todo.exceptions;
-
-public class AuthenticationFailureException extends RuntimeException {
-    public AuthenticationFailureException(String message) {
-        super(message);
-    }
-}
